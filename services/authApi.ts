79fe60e24277c5a18aa807a878d7326a4c6bf5fc
ElderGr <gbielrs@gmail.com>
2023-06-30 17:47:28 -0300

    feat: code review

--- services/authApi.ts
+++
@@ -1,7 +0,0 @@
-import api from './api';
-
-export async function signIn(email:string, password:string) {
-  const response = await api.post('/auth/sign-in', { email, password });
-  return response.data;
-}
-//
